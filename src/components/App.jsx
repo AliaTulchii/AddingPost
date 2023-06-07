e096b79f0d9292322676f2f97c9c3faf59deb716
Halina Tulchii <halendeihalyna@gmail.com>
2023-06-07 12:02:15 +0300

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
+import AddPostForm from "features/AddPostForm/AddPostForm";
 import PostsList from "features/PostsList/PostsList";
 
 export const App = () => {
   return (
     <main className="App">
-      <PostsList/>
+      <AddPostForm/>
+      <PostsList />
     </main>
   );
 };
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,20 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
-    {
-        id: '1', title: 'Learning Redux Toolkit', content: 'I have heard good things.'
+    {id: '1', title: 'Learning Redux Toolkit', content: 'I have heard good things.'
     },
-    {
-        id: '2', title: 'Slices', content: 'The more i say slice, the more i want pizza.'
+    {id: '2', title: 'Slices', content: 'The more i say slice, the more i want pizza.'
     },
 ]
 
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
-    reducers: {}
+    reducers: {
+        postAdded(state, action) {
+            state.push(action.payload);
+        }
+    }
 })
 
 
 export const selectAllPosts = (state) => state.posts;
+export const { postAdded } = postsSlice.actions; 
 export default postsSlice.reducer;
